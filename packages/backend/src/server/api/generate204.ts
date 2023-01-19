32ef935ea62eefae7ae3b27faf523e7fc89e07d5
Matt Moran <mattmoran2000@gmail.com>
2023-01-19 16:43:17 +1300

    Generate 204 should not send a response body

--- packages/backend/src/server/api/generate204.ts
+++ packages/backend/src/server/api/generate204.ts
@@ -5,8 +5,12 @@ export const generate204Route = new GetRouteGenerator({
     requiredParams: [] as const,
     optionalParams: [] as const,
     cacheMaxAge: 0,
-    executor: async (route, { res }) => {
+    executor: (_, { res }) => {
         res.writeStatus("204 No Content");
-        route.finish("success", {});
+        res.writeHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        res.writeHeader("Pragma", "no-cache");
+        res.writeHeader("Expires", "0");
+        res.writeHeader("Access-Control-Allow-Origin", "*");
+        res.endWithoutBody();
     },
 });
