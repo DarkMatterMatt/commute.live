42432c21b749c3585370f12b1626927cc60db99a
Matt Moran <mattmoran2000@gmail.com>
2024-04-28 10:02:17 +1000

    f preconditions

--- packages/common/src/errors/Preconditions.ts
+++ packages/common/src/errors/Preconditions.ts
@@ -1,8 +1,12 @@
 export class PreconditionError extends Error {}
 
-export function checkExists<T>(obj: T | null | undefined): NonNullable<T> {
+/**
+ * Throws error if `obj` is nullish.
+ * @param messageTemplate Error message to throw, replacing `<>` with the stringified object.
+ */
+export function checkExists<T>(obj: T | null | undefined, messageTemplate = "Object is <>"): NonNullable<T> {
     if (obj == null) {
-        throw new PreconditionError(`Object is ${obj}`);
+        throw new PreconditionError(messageTemplate.replace("<>", `${obj}`));
     }
     return obj;
 }
